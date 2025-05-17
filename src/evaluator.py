609eebcae47c4d383fbcd1edea62339828dcf8be
Belluxx <78388441+Belluxx@users.noreply.github.com>
2025-05-17 09:43:59 +0200

    Add dynamic no_think for Qwen3 models

--- src/evaluator.py
+++ src/evaluator.py
@@ -6,8 +6,19 @@ from utils.logger import Logger
 ANSWER_REGEX = r'\\boxed{((?:\\[a-z]+|{[^{}]*}|[^{}])+)}'
 
 
-def ask_llm_aime(llm: LLM, problem: str, prompt: str, max_tokens: int) -> int | None:
-    prompt = f'{problem}\n\n{prompt}\n\n/no_think'
+def ask_llm_aime(
+    llm: LLM,
+    problem: str,
+    prompt: str,
+    max_tokens: int,
+    qwen3_nothink: bool = False,
+    verbose: bool = False
+) -> int | None:
+    prompt = f'{problem}\n\n{prompt}'
+
+    if qwen3_nothink and 'qwen3' in llm.model.lower():
+        prompt += '\n\n/no_think'
+        Logger.info('ask_llm_aime', 'Appending /no_think to prompt for Qwen3', verbose)
 
     response = llm.get_answer(prompt, max_tokens)
 
@@ -20,7 +31,7 @@ def ask_llm_aime(llm: LLM, problem: str, prompt: str, max_tokens: int) -> int | 
             answer = int(match.group(1))
             return answer
         else:
-            Logger.warning('ask_llm', f'The LLM response was not found')
+            Logger.warning('ask_llm_aime', f'The LLM response was not found', verbose)
     except Exception as e:
-        Logger.warning('ask_llm', f'The LLM response was not an integer: {e}')
+        Logger.warning('ask_llm_aime', f'The LLM response was not an integer: {e}', verbose)
         return None
